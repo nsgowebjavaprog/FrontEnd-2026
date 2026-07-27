5957a74236826a9b573c6307d14f70ddf35f404c
ns loni <nsloni@gmail.com>
2026-07-27 15:56:59 +0530

    done

---
+++ ns_js_begineer/1_variables.js
@@ -0,0 +1,42 @@
+// Variables 
+
+// ---- 1 ------ Changable
+let name="Nagaraj"
+let age = 22
+console.log(name, age) // 22
+
+// Changing var-value 
+
+age = 23
+console.log(age) // 23
+
+
+// ---- 2 ------ Not-Changable
+
+const account_num = 4567
+console.log(account_num) // 4567
+// account_num = 1234
+// console.log(account_num)
+
+
+// ---- 3 ------ Changable
+
+var a = 10
+a = 20
+console.log(a)
+
+
+
+// Var [v/s] Let
+
+//without-error
+var a = 10
+var a = 20
+console.log(a)
+
+
+// Error
+/*
+let name="N"
+let name="n"
+*/
---
+++ ns_js_begineer/2_datatypes.js
@@ -0,0 +1,85 @@
+// Data-Types
+
+// 1. Primitive data-type [7-types]
+
+/*
+// 1. Number
+let num_1 = 10
+let num_2 = 10.10
+console.log(num_1, num_2)
+
+// 2. String
+
+let str_1 = "Nagaraj"
+let str_2 = 'Loni'
+console.log(str_1 + str_2)
+
+// 3.Boolean
+
+let is_student = false
+let is_emp = true
+console.log(is_emp, is_student)
+
+// 4. Un-defined
+
+let user;
+console.log(user) // undefined
+
+// 5. Bigint
+let number = 34546767565453423n;
+console.log(number)
+
+// 6. null
+let weather = null;
+console.log(weather)
+
+// 7. symbol
+const id1 = Symbol("id")
+const id2 = Symbol("id")
+console.log(id1)
+console.log(id2)
+console.log(id1 == id2) // false
+*/
+
+// 2. Non-Premitive DataType
+
+/*
+// 1. Array
+
+let arr = [10,20,30,"Loni", true]
+console.log(arr)
+
+
+// 2. Object
+
+let obj = {
+    name: "Loni",
+    account_num: 4567,
+    age: 23,
+    category: 'Male'
+}
+console.log(obj)
+console.log(obj.account_num) // 4567
+
+
+
+function add(){
+    console.log("Loni")
+}
+add()
+
+*/
+/*
+let a = 10
+console.log(typeof(a)) // number
+
+let weather = null
+console.log(typeof weather) // object
+
+function add(){
+    console.log("Loni")
+}
+add()
+console.log(typeof add)
+
+*/

d46dedad68910a97715c84cef1c3c38f8674d003
ns loni <nsloni@gmail.com>
2026-07-28 14:10:12 +0530

    js day-3

--- ns_js_begineer/3_basics.js
+++ ns_js_begineer/3_basics.js
@@ -10,3 +10,31 @@ console.log(Boolean(undefined))  // false
 */
 
 // 24:00 mini
+
+// Maths
+/*
+console.log(Math.PI)
+console.log(Math.abs(-34))
+console.log(Math.SQRT2)
+console.log(Math.sqrt(25))  // 5
+console.log(Math.random())  // 0 to 1
+*/
+
+// Math Random Excesize
+
+// 1 Normal
+console.log(Math.random())
+
+// 2 Range Based
+console.log(Math.random() * 10)
+
+// 3.  1-6
+console.log(Math.floor(Math.random() * 6))
+
+// 4. 50-100 [min-max]
+
+min = 50
+max = 100
+console.log(Math.floor(Math.random() * (max-min+1)) + min)
+
+// crypto for security

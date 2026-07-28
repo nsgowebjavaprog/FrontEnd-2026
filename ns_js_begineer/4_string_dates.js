8b7af8dfd5043cf890123876e275d490fcabbc4b
ns loni <nsloni@gmail.com>
2026-07-28 19:11:57 +0530

    ok

---
+++ ns_js_begineer/4_string_dates.js
@@ -0,0 +1,19 @@
+// String
+/*
+const str1 = "Loni"
+const str2 = 'Loni'
+const str3 = `it can
+allow us to write in
+1 to n line also`
+*/
+
+// console.log(str1, str2, str3)
+
+const date = 20
+const name = `Loni`
+
+console.log(`today date is ${date}`)
+
+console.log(name.length) // 4
+
+
